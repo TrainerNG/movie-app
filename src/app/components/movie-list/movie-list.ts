58ca1eadd87f3a47284b4b1279032936d21aa1b7
apple <apple@Apples-MacBook-Air.local>
2025-12-22 17:36:01 +0530

    performance1

--- src/app/components/movie-list/movie-list.ts
+++ src/app/components/movie-list/movie-list.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, computed, inject, signal } from '@angular/core';
 import { Movie } from '../../services/movie';
 import { MovieSearchResponse } from '../../interfaces/movie-search-response';
 import { FormsModule } from '@angular/forms';
@@ -9,6 +9,7 @@ import { EncryptionService } from '../../services/encryption-service';
 import { ToastService } from '../../services/toast-service';
 import { MovieCard } from "../movie-card/movie-card";
 import { Loading } from '../../services/loading';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movie-list',
@@ -17,70 +18,100 @@ import { Loading } from '../../services/loading';
   styleUrl: './movie-list.css',
 })
 export class MovieList {
-private movieService = inject(Movie);
-private cdr = inject(ChangeDetectorRef);
-private encryptionService = inject(EncryptionService);
-private toastrService = inject(ToastService);
-loadingService = inject(Loading);
-private router = inject(Router);
+  private movieService = inject(Movie);
+  private cdr = inject(ChangeDetectorRef);
+  private encryptionService = inject(EncryptionService);
+  private toastrService = inject(ToastService);
+  loadingService = inject(Loading);
+  private router = inject(Router);
 
-query = '';
-currentPage = 1;
-movies:MovieInterface[]=[];
-loading = false;
-totalResults = 0;
-error = false;
+  readonly query = signal('');
+  readonly currentPage = signal(1);
+  readonly movies = signal<MovieInterface[]>([]);
+  readonly totalResults = signal(0);
 
-ngOnInit(){
-  this.loadPopularMovies();
-}
+  readonly isLoading$ = this.loadingService.isLoading$;
 
-search(){
-  if(this.query){
-    this.currentPage = 1;
-   this.loadMovies();
-  }else{
+  ngOnInit() {
     this.loadPopularMovies();
   }
-}
 
-loadMovies(){
-  this.movieService.searchMovies(this.query, this.currentPage).subscribe({
-    next:(response: MovieSearchResponse)=>{
-      this.movies = response.results || [];
-      this.totalResults = response.total_results;
-      this.cdr.markForCheck();
+  search() {
+    if (this.query().trim()) {
+      this.currentPage.set(1);
+      this.loadMovies();
+    } else {
+      this.loadPopularMovies();
     }
-  })
-}
+  }
 
-loadPopularMovies(){
-  this.movieService.getPopularMovies(this.currentPage).subscribe({
-    next:(response: MovieSearchResponse) => {
-      this.movies = response.results || [];
-      this.totalResults = response.total_results;
-      this.cdr.markForCheck() // FORCEFULLY UPDATING UI.
-    },
-    error:(err)=>{
-      console.error('loadPopularMovies error: ', err);
-      this.error = true;
-      this.cdr.markForCheck();
+  async loadMovies() {
+    // this.movieService.searchMovies(this.query, this.currentPage).subscribe({
+    //   next:(response: MovieSearchResponse)=>{
+    //     this.movies = response.results || [];
+    //     this.totalResults = response.total_results;
+    //     this.cdr.markForCheck();
+    //   }
+    // })
+    await this.resolveRequest(this.movieService.searchMovies(this.query().trim(), this.currentPage()));
+  }
 
+  async loadPopularMovies() {
+    // this.movieService.getPopularMovies(this.currentPage).subscribe({
+    //   next:(response: MovieSearchResponse) => {
+    //     this.movies = response.results || [];
+    //     this.totalResults = response.total_results;
+    //     this.cdr.markForCheck() // FORCEFULLY UPDATING UI.
+    //   },
+    //   error:(err)=>{
+    //     console.error('loadPopularMovies error: ', err);
+    //     this.error = true;
+    //     this.cdr.markForCheck();
+
+    //   }
+    // })
+
+    await this.resolveRequest(this.movieService.getPopularMovies(this.currentPage()))
+
+  }
+
+  private async resolveRequest(request: Observable<MovieSearchResponse>) {
+    try {
+      const response = await firstValueFrom(request);
+      this.movies.set(response.results ?? []);
+      this.totalResults.set(response.total_results ?? 0);
+    } catch (err) {
+      this.movies.set([]);
+      this.totalResults.set(0);
     }
+  }
+
+  // get totalPages(): number{
+  //   return Math.ceil(this.totalResults / 20) ; // TMDB has 20 pages
+  // }
+
+  readonly totalPages = computed(() => {
+    const results = this.totalResults();
+    return results >= 0 ? Math.ceil(results / 20) : 0
   })
-}
 
-get totalPages(): number{
-  return Math.ceil(this.totalResults / 20) ; // TMDB has 20 pages
-}
+  onPageChange(page: number) {
+    // this.currentPage = page;
+    // this.loadPopularMovies();
+    if (page < 1 || (this.totalPages() && page > this.totalPages())) {
+      return;
+    }
+    this.currentPage.set(page);
+    if (this.query().trim()) {
+      this.loadMovies();
+    }
+    else {
+      this.loadPopularMovies();
+    }
+  }
 
-onPageChange(page: number){
-  this.currentPage = page;
-  this.loadPopularMovies();
-}
-
-viewDetails(movieId: number){
-  const encryptedId = this.encryptionService.encrypt(movieId.toString());
-this.router.navigate(['/movie',encryptedId]);
-}
+  viewDetails(movieId: number) {
+    const encryptedId = this.encryptionService.encrypt(movieId.toString());
+    this.router.navigate(['/movie', encryptedId]);
+  }
 }
